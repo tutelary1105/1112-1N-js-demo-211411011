5345e7bd194925c8c009e56daf47eeac5155a5ab
tutelary1105 <tutelary1105@gmail.com>
2023-02-23 20:34:35 +0800

    W02-P2: O win (9 times), X wins (8 times), tie (9 times)

--- demo/w02.tictactoe/tictactoe-starter/tictactoe_11.js
+++ demo/w02.tictactoe/tictactoe-starter/tictactoe_11.js
@@ -1,10 +1,17 @@
+
+const o = 'o';
+const x = 'x';
+let turn = 0;
+let done = false;
+
 const resetBtn = document.querySelector('#reset');
 
 const allLi = document.querySelectorAll('#board li');
 console.log('allLi', allLi);
 
-const checkWin = (player) => {
+const container = document.querySelector('#container');
 
+const checkWin = (player) => {
   let p = [];
   allLi.forEach( (item) => {
     p.push(item.classList.contains(player));
@@ -36,10 +43,51 @@ const reset = () => {
   allLi.forEach( (item) => {
     item.textContent = '+';
     item.classList = '';
+    container.style.backgroundColor = '#666';
+    turn = 0;
+    done = false;
   });
 }
 
-console.log('checkWin(o)', checkWin('o'));
-console.log('checkWin(x)', checkWin('x'));
+const winMessage = (player) => {
+  if(player === 'o'){
+    container.style.backgroundColor = 'rgba(144, 238, 144, 0.5)';
+  } else {
+    container.style.backgroundColor = 'rgba(240, 118, 128, 0.726)';
+  }
+  alert(`player ${player} wins`);
+}
+
+
+
+allLi.forEach( (item) => {
+    item.addEventListener('click', () => {
+        if( item.classList.contains('disabled') ) {
+          alert('Already filled');
+        } else if(turn % 2 === 0){
+            item.textContent = 'O';
+            item.classList.add('o', 'disabled');
+            if(checkWin(o)){
+                winMessage(o);
+                done = true;
+            }
+        } else if(turn % 2 == 1){
+            item.textContent = 'X';
+            item.classList.add('x', 'disabled');
+            if(checkWin(x)){
+                winMessage(x);
+                done = true;
+            }
+        }
+        if(!done && turn < 8) {
+            turn++;
+        } else if( turn >=8) {
+            alert('tie');
+        }
+    } );
+});
+
+//console.log('checkWin(o)', checkWin('o'));
+//console.log('checkWin(x)', checkWin('x'));
 
 resetBtn.addEventListener('click', reset);
