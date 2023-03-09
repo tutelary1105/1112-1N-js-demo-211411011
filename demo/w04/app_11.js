1d3d8c0f6e24e8557408c714299f4539e78f0ed7
tutelary1105 <tutelary1105@gmail.com>
2023-03-09 19:15:34 +0800

    W04-P1: add 3 images of my meals to menu array

---
+++ demo/w04/app_11.js
@@ -0,0 +1,60 @@
+const menu = [
+    {
+        id: 1,
+        title: "bamburger",
+        category: 'breakfast',
+        price: 11.49,
+        img: './images/my-1.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+    {
+        id: 2,
+        title: "steak",
+        category: 'breakfast',
+        price: 16.99,
+        img: './images/my-2.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+    {
+        id: 3,
+        title: "salmon",
+        category: 'breakfast',
+        price: 12.79,
+        img: './images/my-2.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+    {
+        id: 4,
+        title: "buttermilk pancakes",
+        category: 'breakfast',
+        price: 15.59,
+        img: './images/item-1.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+    {
+        id: 5,
+        title: "dinner double",
+        category: 'dinner',
+        price: 13.99,
+        img: './images/item-2.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+    {
+        id: 6,
+        title: "godzilla milkshake",
+        category: 'shakes',
+        price: 13.99,
+        img: './images/item-3.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam'
+    },
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('btn-container');
+
