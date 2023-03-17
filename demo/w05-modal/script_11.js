61b0261e6b719ff4e514e1223ff08c1a3bb3f3f1
tutelary1105 <tutelary1105@gmail.com>
2023-03-17 17:02:34 +0800

    W05-P4: call closeModal to close modal, overlay; press ESC key will call closeModal as well01~

--- demo/w05-modal/script_11.js
+++ demo/w05-modal/script_11.js
@@ -1,24 +1,52 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const esc = document.querySelector('.overlay');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log('modal', modal);
 console.log('show-modal', btnsOpenModal);
 
 
-const openModal = (index) => {
+const openModal = () => {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
 
-for(let i=0; i<btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
+const closeModal = () => {
+    modal.forEach((item) => {
+        item.classList.add('hidden');
+    })
+    console.log('close.')
+    overlay.classList.add('hidden');
+    console.log('This section has been closed.');
 }
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ESCAPE' && !modal.classList.contains('hidden')) {
+
+for(let i=0; i<btnsOpenModal.length; i++ ){
+    btnsOpenModal[i].addEventListener('click', ()=> {
+        console.log(modal[i]);
+        modal[i].classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+for(let i=0; i<btnCloseModal.length; i++ ){
+    btnCloseModal[i].addEventListener('click', ()=> {
+        console.log(modal[i]);
+        modal[i].classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+}
+
+
+overlay.addEventListener('click',closeModal);
+
+
+//click滑鼠事件
+document.addEventListener('keydown', (e) =>{
+    if (e.key ==='Escape' && !overlay.classList.contains('hidden')){
         closeModal();
     }
 });
