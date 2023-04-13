f6c8bc76b903e50f84cb4b05358e7dcb339a4bb6
tutelary1105 <tutelary1105@gmail.com>
2023-04-13 20:07:16 +0800

    W09-P1: Midprep P1 answer (a)(b)

---
+++ demo/w09/p3_tour_11/p3_11.js
@@ -0,0 +1,9 @@
+let tours_xx = [];
+
+const displayTours_xx = ({ tours }) => {};
+
+const fetchTours_xx = async () => {};
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetchTours_xx(url);
+});
