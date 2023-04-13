6861ce69d6136d3f8b709219c553a2fedfcce432
tutelary1105 <tutelary1105@gmail.com>
2023-04-13 21:55:29 +0800

    .

--- js/classdemo_11.js
+++ js/classdemo_11.js
@@ -2,10 +2,10 @@ const showClassDemo = (week) => {
     const showDemo = document.querySelector('.banner-section');
     switch(week){
         case 'w1':
-            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100vh"/>`
+            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100%"/>`
             break;
         case 'w2':
-            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100vh"/>`
+            showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100%"/>`
             break;
     }
 }
