a2cde2e27d860e25cf1cdce4edc9e14596c7e1d4
tutelary1105 <tutelary1105@gmail.com>
2023-05-04 21:45:45 +0800

    W12-P5: xhr, get people.json, and show names in browser

--- demo/w12/7-async-tutorials/app.js
+++ demo/w12/7-async-tutorials/app.js
@@ -1,21 +1,31 @@
-const xhr = new XMLHttpRequest();
+const btn = document.querySelector('.btn');
+const url = './api/people.json';
 
-xhr.open('GET', './api/sample2.txt')
+btn.addEventListener('click', () => {
+    getData(url);
+});
 
-xhr.onreadystatechange = function () {
-    console.log('xhr', xhr);
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        const text = document.createElement('p');
-        console.log('p', text);
-        text.textContent = xhr.responseText;
-        document.body.appendChild(text);
-    } else {
-        console.log({
-            status: xhr.status,
-            text: xhr.statusText,
-            readyState: xhr.readyState
-        });
+const getData = (url) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url)
+    xhr.onreadystatechange = function () {
+        console.log('xhr', xhr);
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            console.log('data', data);
+            const displayData = data.map((item) => {
+                return `<p> ${item.name} </p>`
+            }).join('');
+            const divElement =document.createElement('div');
+            divElement.innerHTML = displayData;
+            document.body.appendChild(divElement);
+        } else {
+            console.log({
+                status: xhr.status,
+                text: xhr.statusText,
+                readyState: xhr.readyState
+            });
+        }
     }
+    xhr.send();
 }
-
-xhr.send();
