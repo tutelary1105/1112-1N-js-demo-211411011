8ce93d58068ce304325cca36fc378e7ba18ccaba
tutelary1105 <tutelary1105@gmail.com>
2023-05-29 04:41:54 +0800

    W14-P2: Get data from MealDB, SportDB, AudioDB one by one as in P1

--- demo/w14-api-more/app_11.js
+++ demo/w14-api-more/app_11.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', async () => {
-  const url = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
+  const url = 'https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=a';
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log('data', data.meals);
-    displayItems(data.meals);
+    console.log('data', data.player);
+    displayItems(data.player);
   } catch (error) {
     console.error(error);
   }
@@ -16,8 +16,8 @@ btn.addEventListener('click', async () => {
 const displayItems = (items) => {
   const displayData = items
     .map((item) => {
-      const { strMeal } = item;
-      return `<p>${strMeal}</p>`;
+      const { strPlayer } = item;
+      return `<p>${strPlayer}</p>`;
     })
     .join('');
   const element = document.createElement('div');
