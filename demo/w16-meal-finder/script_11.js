2af49e6fa8b73ec42bf91472d57d69a2b8875049
tutelary1105 <tutelary1105@gmail.com>
2023-06-01 21:32:15 +0800

    W16-P2: Show single meal by its ID

--- demo/w16-meal-finder/script_11.js
+++ demo/w16-meal-finder/script_11.js
@@ -14,7 +14,7 @@ const searchMeal = (e) => {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
         .then(response => response.json())
         .then(data => {
-            console.log('data',data);
+            console.log('meals data',data);
             
             if(data.meals === null){
                 resultHeading.innerHTML = `<p> There are no search results. Try again! </p>`
@@ -23,21 +23,75 @@ const searchMeal = (e) => {
                 mealsEl.innerHTML = data.meals.map((meal)=>{
                     return `
                         <div class="meal">
-                            <img src="${meal.strMealThumb}" data-mealID="${meal.idMeal}" />
-                            <div class="meal-info">
+                            <img src="${meal.strMealThumb}" />
+                            <div class="meal-info" data-mealid="${meal.idMeal}">
                                 <h3> ${meal.strMeal} </h3>
                             </div>
                         </div>
                     `
                 }).join('');
+
             }
         })
     } else {
-        alert('Please enter a search term');
+        // alert('Please enter a search term');
     }
 }
 
+const getMealById = (mealID) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log('single meal',data);
+        const meal = data.meals[0];
+        addMealToDOM(meal);
+    })
+}
 
+const addMealToDOM = (meal) => {
+    const ingredients = [];
+    for(let i=1; i<=20; i++) {
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+    console.log('ingredients', ingredients);
+
+    single_mealEl.innerHTML = `
+        <div class="single-meal">
+            <h1> ${meal.strMeal} </h1>
+            <img src="${meal.strMealThumb}" />
+            <div class="main">
+                <p> ${meal.strInstructions} </p>
+                <h2> Ingredients </h2>
+                <ul>
+                    ${ingredients.map(ing => {
+                        return `<li>${ing}</li>`
+                    }).join('')}
+                </ul>
+            </div>
+        </div>
+    `
+}
 
 // Event listeners
 submit.addEventListener('click', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+    // console.log('e.path', e.composedPath());
+    const composedPath = e.composedPath();
+    const mealInfo = composedPath.find( item => {
+        if(item.classList){
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+    if(mealInfo) {
+        // console.log('mealInfo', mealInfo);
+        const mealID = mealInfo.getAttribute('data-mealid');
+        getMealById(mealID);
+    }
+});
